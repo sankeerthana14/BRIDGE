190bb2b8da1115731e1c2478a4a5b0dc216407b8
sankeerthana14 <sankeerthana.satini3@gmail.com>
2026-07-22 19:57:10 +0800

    created folders

---
+++ PATHS.py
@@ -0,0 +1,12 @@
+import os
+
+CWD = os.getcwd()
+
+BASELINES_DIR = os.path.join(CWD, "baselines")
+FIGURES_DIR = os.path.join(CWD, "figures")
+LOGS_DIR = os.path.join(CWD, "logs")
+MODELS_DIR = os.path.join(CWD, "models")
+RESULTS_DIR = os.path.join(CWD, "results")
+SCRIPTS_DIR = os.path.join(CWD, "scripts")
+SLURM_DIR = os.path.join(CWD, "slurm")
+SRC_DIR = os.path.join(CWD, "src")
